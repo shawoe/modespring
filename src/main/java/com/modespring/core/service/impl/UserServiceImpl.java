ea5a660c2f3819e748a7f67ab169ae5304e6db1d
shawoe <shawoe@163.com>
2015-04-19 22:16:59 +0800

    创建 UserService

--- src/main/java/com/modespring/core/service/impl/UserServiceImpl.java
+++ src/main/java/com/modespring/core/service/impl/UserServiceImpl.java
@@ -1,17 +1,22 @@
 package com.modespring.core.service.impl;
 
 import com.modespring.core.domain.User;
-//import javax.transaction.Transactional;
+import com.modespring.core.repository.UserDao;
+import com.modespring.core.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 
 /**
  * Created by Shawoe on 2015/4/18.
  */
-//@Transactional
-public class UserServiceImpl {
+@Service
+@Transactional
+public class UserServiceImpl extends BaseService{
 
-    //@Transactional
-    public void addUser(User user) {
-
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        super(userDao);
     }
 
 }
