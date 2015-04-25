4a15ec8062c29aa192bff4827cc4e61ff1ef6375
shawoe <shawoe@163.com>
2015-04-25 10:22:47 +0800

    创建 UserController 测试通过

--- src/main/java/com/modespring/core/webapp/UserController.java
+++ src/main/java/com/modespring/core/webapp/UserController.java
@@ -1,9 +1,12 @@
 package com.modespring.core.webapp;
 
+import com.modespring.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Shawoe on 2015/4/24.
@@ -11,12 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
-//    @Autowired
-//    public UserService userService;
+    @Autowired
+    public UserService userService;
 
     @RequestMapping(value = "/hello.html", method = RequestMethod.GET)
-    public ModelAndView hello(String name, ModelAndView modelAndView) {
-        modelAndView.addObject("name", name);
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.addObject("name", request.getParameter("name"));
         return modelAndView;
     }
 
---
+++ src/test/java/com/modespring/core/webapp/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.modespring.core.webapp;
+
+/**
+ * Created by Shawoe on 2015/4/25.
+ */
+public class UserControllerTest {
+}
