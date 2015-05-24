046cc42b883e31dbf70cb34082a7b91c299503e5
shawoe <shawoe@163.com>
2015-05-24 14:03:34 +0800

    删除无效测试

--- src/test/java/com/modespring/core/repository/RepoTest.java
+++ src/test/java/com/modespring/core/repository/RepoTest.java
@@ -1,17 +1,11 @@
 package com.modespring.core.repository;
 
-import com.modespring.core.domain.Article;
-import com.modespring.core.domain.Role;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * Created by Shawoe on 2015/4/30.
@@ -19,18 +13,8 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration("/application-context.xml")
-@TransactionConfiguration(defaultRollback = false)
+@TransactionConfiguration(defaultRollback = true)
 @Transactional
-public class AtricleDaoTest {
+public class RepoTest {
 
-    @Autowired
-    ArticleDao articleDao;
-
-
-    @Test
-    public void test() {
-//        List<Article> articleList = articleDao.findByColumnId(2);
-//        System.out.println("--------------------------------------------"+articleList);
-
-    }
 }
--- src/test/java/com/modespring/core/webapp/UserControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.modespring.core.webapp;
-
-/**
- * Created by Shawoe on 2015/4/25.
- */
-public class UserControllerTest {
-}
